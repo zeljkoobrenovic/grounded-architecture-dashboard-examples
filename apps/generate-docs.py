cea4378096b1c25371678ee4751ee0916a8f086c
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2024-07-05 12:55:07 +0200

    capability map

--- apps/generate-docs.py
+++ apps/generate-docs.py
@@ -1,9 +1,11 @@
 import json
 
-data = json.load(open('data/apps.json'))
+active = json.load(open('data/active.json'))
+archived = json.load(open('data/archived.json'))
 template = open('templates/apps.html').read();
 
 htmlFile = 'docs/index.html'
 print(htmlFile)
 with open(htmlFile, 'w') as html_file:
-    html_file.write(template.replace('${data}', json.dumps(data)))
+    html_file.write(template.replace('${active}', json.dumps(active))
+                    .replace('${archived}', json.dumps(archived)))
